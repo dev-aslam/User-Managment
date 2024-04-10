2cdfce62077e814e9b357537427c870bb0f52858
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-04-10 19:47:11 +0530

    added bg image

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path')
 const port = 8080;
 
 app.set('view engine','ejs')
+app.use(express.static(__dirname+'/assets'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
-    res.send("hello world");
+    res.render("Login",{variable_name:"Hello World"})
 })
 
 app.listen(port,(req,res)=>{
